90d084e7c581d889e57856c13c449e9225c86c8d
ichn-hu <ichneumon.hu@foxmail.com>
2017-11-29 16:12:01 +0800

    ... try to reconstruct

--- backend/cpu.py
+++ backend/cpu.py
@@ -6,91 +6,92 @@ from register import Register
 from misc import split2chunks
 
 class Unit:
-    def __init__(_, mem):
-        _.mem = mem
-        _.reg = Register()
-        _.F_predPC = ZERO
-        _.f_stat = SAOK
-        _.imem_error = False
-        _.instr_valid = True
-#        _.f_pc
-#        _.f_predPC
+    def __init__(self, mem):
+        self.mem = mem
+        self.reg = Register()
+        self.F_predPC = ZERO
+        self.f_stat = SAOK
+        self.imem_error = False
+        self.instr_valid = True
+#        self.f_pc
+#        self.f_predPC
 
-        _.D_stat = SAOK
-        _.D_icode = INOP
-        _.D_ifun = FNONE
-        _.D_rA = RNONE
-        _.D_rB = RNONE
-        _.D_valC = ZERO
-        _.D_valP = ZERO
-        _.d_rvalA
-        _.d_srcA
-        _.d_srcB
+        self.D_stat = SAOK
+        self.D_icode = INOP
+        self.D_ifun = FNONE
+        self.D_rA = RNONE
+        self.D_rB = RNONE
+        self.D_valC = ZERO
+        self.D_valP = ZERO
+        self.d_rvalA
+        self.d_srcA
+        self.d_srcB
 
-        _.E_stat
-        _.E_icode
-        _.E_ifun
-        _.E_valC
-        _.E_valA
-        _.E_valB
-        _.E_dstE
-        _.E_dstM
-        _.E_srcA
-        _.E_srcB
-        _.e_Cnd
+        self.E_stat
+        self.E_icode
+        self.E_ifun
+        self.E_valC
+        self.E_valA
+        self.E_valB
+        self.E_dstE
+        self.E_dstM
+        self.E_srcA
+        self.E_srcB
+        self.e_Cnd
+        self.e_dstM
 
-        _.M_stat
-        _.M_icode = INOP
-        _.M_Cnd = False
-        _.M_valE = ZERO
-        _.M_valA = ZERO
-        _.M_dstE = RNONE
-        _.M_dstM = RNONE
-        _.m_stat
-        _.dmem_error
+        self.M_stat
+        self.M_icode = INOP
+        self.M_Cnd = False
+        self.M_valE = ZERO
+        self.M_valA = ZERO
+        self.M_dstE = RNONE
+        self.M_dstM = RNONE
+        self.m_stat
+        self.dmem_error
 
-        _.W_stat
-        _.W_icode
-        _.W_valE
-        _.W_valM = ZERO
-        _.W_dstE
-        _.W_dstM
+        self.W_stat
+        self.W_icode
+        self.W_valE
+        self.W_valM = ZERO
+        self.W_dstE
+        self.W_dstM
 
         
-    def fetch(_):
+    def fetch(self):
         info = {}
         info["process"] = []
 
 
-        if _.M_icode == IJXX and not _.M_Cnd:
-            f_pc = _.M_valA
-        elif _.M_icode == IRET:
-            f_pc = _.W_valM
+        if self.M_icode == IJXX and not self.M_Cnd:
+            f_pc = self.M_valA
+        elif self.M_icode == IRET:
+            f_pc = self.W_valM
         else:
-            f_pc = _.F_predPC
-        _.imem_error = False
+            f_pc = self.F_predPC
+        self.imem_error = False
         try:
-            icode, ifun = split2chunks(_.mem.read(f_pc, 1), 1)
+            icode, ifun = split2chunks(self.mem.read(f_pc, 1), 1)
         except:
-            _.imem_error = True
+            self.imem_error = True
             ifun = FNONE
             icode = INOP
-        _.instr_valid = ifun in [ INOP, IHALT, IRRMOVL, IIRMOVL, IRMMOVL, IMRMOVL, \
+        self.instr_valid = ifun in [ INOP, IHALT, IRRMOVL, IIRMOVL, IRMMOVL, IMRMOVL, \
                                 IOPL, IJXX, ICALL, IRET, IPUSHL, IPOPL]
-        if _.imem_error:
+        if self.imem_error:
             f_stat = SADR
-        elif not _.instr_valid:
-            _.f_stat = SINS
+        elif not self.instr_valid:
+            self.f_stat = SINS
         elif icode == IHALT:
-            _.f_stat = SHLT
+            self.f_stat = SHLT
         else:
-            _.f_stat = SAOK
+            self.f_stat = SAOK
 
         need_regid = icode in [IRRMOVL, IRMMOVL, IMRMOVL, IIRMOVL, IPUSHL, IPOPL, IIADDL]
         need_valC = icode in [IIRMOVL, IRMMOVL, IMRMOVL, IJXX, ICALL, IIADDL]
-        valP = _.F_predPC + 1 * int(need_regid) + 4 * int(need_valC)
+        valP = self.F_predPC + 1 * int(need_regid) + 4 * int(need_valC)
         if need_valC:
-            valC = _.mem.read(f_pc + 1 + int(need_regid), 4)
+            valC = self.mem.read(f_pc + 1 + int(need_regid), 4)
         else:
             valC = None
 
@@ -100,28 +101,104 @@ class Unit:
             f_predPC = valP
 
         if need_regid:
-            rA, rB = split2chunks(_.mem.read(f_pc + 1, 1), 1)
+            rA, rB = split2chunks(self.mem.read(f_pc + 1, 1), 1)
         else:
             rA, rB = RNONE, RNONE
 
         # 应该在下一个Clock来的时候由f_predPC更新, 不过管他呢233
         # programmer visible state 不会有区别的(蜜汁自信
+        # 执行的时候是倒着来的, WMEDF
         
-        _.F_predPC = f_predPC
-        _.D_valC = valC
-        _.D_valP = valP
-        _.D_rA = rA
-        _.D_rB = rB
-        _.D_icode = icode
-        _.D_ifun = ifun
-        _.D_stat = f_stat
+        self.F_predPC = f_predPC
+        self.D_valC = valC
+        self.D_valP = valP
+        self.D_rA = rA
+        self.D_rB = rB
+        self.D_icode = icode
+        self.D_ifun = ifun
+        self.D_stat = f_stat
 
         return info
-    def decode(_):
+
+    def decode(self):
+        info = {}
+        info["process"] = []
+        if self.D_icode in [IRRMOVL, IRMMOVL, IOPL, IPUSHL]:
+            d_srcA = self.D_rA
+        elif self.D_icode in [IPOPL, IRET]:
+            d_srcA = RESP
+        else:
+            d_srcA = RNONE
+        
+        if self.D_icode in [IOPL, IRMMOVL, IMRMOVL]:
+            d_srcB = self.D_rB
+        elif self.D_icode [IPUSHL, IPOPL, ICALL, IRET]:
+            d_srcB = RESP
+        else:
+            d_srcB = RNONE
+        
+        if self.D_icode in [IRRMOVL, IIRMOVL, IOPL]:
+            d_dstE = self.D_rB
+        elif self.D_icode in [IPOPL, IPOPL, ICALL, IRET]:
+            d_dstE = RESP
+        else:
+            d_dstE = RNONE
+        
+        if self.D_icode in [IMRMOVL, IPOPL]:
+            d_dstM = self.D_rB
+        else:
+            d_dstM = RNONE
+        
+        d_rvalA, d_rvalB = self.reg.read(d_srcA, d_srcB)
+
+        if self.D_icode in [ICALL, IJXX]:
+            d_valA = self.D_valP
+        elif d_srcA == self.e_dstE:
+            d_valA = self.e_valE
+        elif d_srcA == self.M_dstM:
+            d_valA = self.m_valM
+        elif d_srcA == self.M_dstE:
+            d_valA = self.M_valE
+        elif d_srcA == self.W_dstM:
+            d_valA = self.W_valM
+        elif d_srcA == self.W_dstE:
+            d_valA = self.W_valE
+        else:
+            d_valA = d_rvalA
+
+        if d_srcB == self.e_dstE:
+            d_valB = self.e_valE
+        elif d_srcB == self.M_dstM:
+            d_valB = self.m_valM
+        elif d_srcB == self.M_dstE:
+            d_valB = self.M_valE
+        elif d_srcB == self.W_dstM:
+            d_valB = self.W_valM
+        elif d_srcB == self.W_dstE:
+            d_valB = self.W_valE
+        else:
+            d_valB = d_rvalB
+        
+        # update pipeline register on Excute
+
+        self.E_stat = self.D_stat
+        self.E_icode = self.D_icode
+        self.E_ifun = self.D_ifun
+        self.E_valC = self.D_valC
+        self.E_valA = 
+        
+
+
+        
+
+
+];
+
+
+        return info
+    def excute(self):
         pass
-    def excute(_):
+    def memory(self):
         pass
-    def memory(_):
-        pass
-    def writeBack(_):
+    def writeBack(self):
         pass

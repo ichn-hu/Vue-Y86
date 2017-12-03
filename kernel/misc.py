2d1a54a445b7d208d76f3151239403b83b320f1f
ichn-hu <zfhu16@fudan.edu.cn>
2017-12-03 13:11:29 +0800

    ADD all

--- kernel/misc.py
+++ kernel/misc.py
@@ -1,13 +1,17 @@
 from kernel.const import *
 
+
 def split2chunks(s, l):
-    return [s[i : i + l] for i in range(0, len(s), l)]
+    return [s[i: i + l] for i in range(0, len(s), l)]
+
 
 def int16(v):
     return int(v, 16)
 
+
 def toInteger(v):
     return int(swichEndian(v), 16)
+
 
 def swichEndian(s):
     if s.startswith("0x") or s.startswith("0X"):
@@ -16,6 +20,7 @@ def swichEndian(s):
         s = s[-8:]
     s = "0" * (8 - len(s)) + s
     return ''.join(reversed(split2chunks(s, 2)))
+
 
 def toHex(val):
     """
@@ -40,12 +45,19 @@ def toHex(val):
         s = hex(val)
     return s
 
+
 def aluAdd(a, b, c, cc):
     pass
+
+
 def aluSub(a, b, c, cc):
     pass
+
+
 def aluAnd(a, b, c, cc):
     pass
+
+
 def aluXor(a, b, c, cc):
     pass
 
@@ -53,24 +65,29 @@ def aluXor(a, b, c, cc):
 def d2h(d):
     return "0123456789abcdef"[int(d)]
 
+
 def disassemble(icode, ifun, rA, rB, valC):
     ins = d2h(icode) + d2h(ifun)
-    ret = instrName[ins]
-    if icode in [IRRMOVL, IOPL]:
-        ret += ' %s, %s' % (regName[rA], regName[rB])
-    elif icode in [IIRMOVL]:
-        ret += ' %s, %s' % (swichEndian(valC), regName[rB])
-    elif icode in [IRMMOVL]:
-        ret += ' %s, %s(%s)' % (regName[rA], str(toInteger(valC)), regName[rB])
-    elif icode in [IMRMOVL]:
-        ret += ' %s(%s), %s' % (str(toInteger(valC)), regName[rB], regName[rA])
-    elif icode in [IJXX, ICALL]:
-        ret += ' %s' % (swichEndian(valC))
-    elif icode in [IPOPL, IPUSHL]:
-        ret += ' %s' % (regName[rA])
+    try:
+        ret = instrName[ins]
+        if icode in [IRRMOVL, IOPL]:
+            ret += ' %s, %s' % (regName[rA], regName[rB])
+        elif icode in [IIRMOVL]:
+            ret += ' %s, %s' % (swichEndian(valC), regName[rB])
+        elif icode in [IRMMOVL]:
+            ret += ' %s, %s(%s)' % (regName[rA],
+                                    str(toInteger(valC)), regName[rB])
+        elif icode in [IMRMOVL]:
+            ret += ' %s(%s), %s' % (str(toInteger(valC)),
+                                    regName[rB], regName[rA])
+        elif icode in [IJXX, ICALL]:
+            ret += ' %s' % (swichEndian(valC))
+        elif icode in [IPOPL, IPUSHL]:
+            ret += ' %s' % (regName[rA])
+    except:
+            ret = 'Invalid'
     return ret
 
 
 if __name__ == "__main__":
     print(disassemble(IMRMOVL, 0, 0, 0, 1, 2, '30303333'))
-

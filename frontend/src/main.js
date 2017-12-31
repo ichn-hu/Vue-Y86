0d1df8a72b80665a116e7c81150d23341f932f7e
ichn-hu <zfhu16@fudan.edu.cn>
2017-12-31 13:47:12 +0800

    last commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,27 +11,9 @@ Vue.use(Router)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-var myVue = new Vue({
+new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
 })
-
-document.onkeyup = function (evt) {
-  var e = evt || window.evt
-  if (e && !myVue.$data.uploadAllowed) {
-    if (e.keyCode === 13 || e.keyCode === 82) {
-      myVue.renderInit()
-    } else if (e.keyCode === 37 || e.keyCode === 80) {
-      myVue.renderPrevious()
-    } else if (e.keyCode === 39 || e.keyCode === 78) {
-      myVue.renderNext()
-    } else if (e.keyCode === 32) {
-      myVue.$data.stopped = !myVue.$data.stopped
-      if (!myVue.$data.stopped) {
-        myVue.runThis()
-      }
-    }
-  }
-}
